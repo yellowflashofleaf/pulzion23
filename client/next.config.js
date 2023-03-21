bc7850d408df1806c61baae26588d02b97395f11
= <=>
2023-03-21 20:08:12 +0530

    Build

--- client/next.config.js
+++ client/next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["i.imgur.com", "pulzion-website-images.s3.ap-south-1.amazonaws.com"],
+    domains: ["i.imgur.com", "pulzion-website-images.s3.ap-south-1.amazonaws.com", "cdn.svgator.com"],
   },
 };
 
