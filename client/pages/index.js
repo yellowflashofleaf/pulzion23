28ab080391e83e2af51f798998bc12155b2a410a
Anand Lunawat <aplunawat@gmail.com>
2023-04-24 00:40:14 +0530

    home page changes

--- client/pages/index.js
+++ client/pages/index.js
@@ -6,41 +6,39 @@ import AppContext from "../context/AppContext";
 import { useRouter } from "next/router";
 import Head from "next/head";
 import { IoCalendarOutline } from "react-icons/io5";
-// import home.module.css
+import styles from "../styles/homeAnimation.module.css"
 export default function Home() {
   const { user } = useContext(AppContext);
   const [exploring, setExploring] = useState(false);
   const router = useRouter();
-
-  useEffect(() => {
-    let button = document.querySelector(".primary-button");
-    let item = document.querySelector(".primary-button .round");
-    button.addEventListener("mouseenter", function (event) {
-      this.classList += " animate";
-      let buttonX = event.offsetX;
-      let buttonY = event.offsetY;
-      if (buttonY < 24) {
-        item.style.top = 0 + "px";
-      } else if (buttonY > 30) {
-        item.style.top = 48 + "px";
-      }
-      item.style.left = buttonX + "px";
-      item.style.width = "1px";
-      item.style.height = "1px";
-    });
-    button.addEventListener("mouseleave", function () {
-      this.classList.remove("animate");
-      let buttonX = event.offsetX;
-      let buttonY = event.offsetY;
-      if (buttonY < 24) {
-        item.style.top = 0 + "px";
-      } else if (buttonY > 30) {
-        item.style.top = 48 + "px";
-      }
-      item.style.left = buttonX + "px";
-    });
-  }, []);
-
+  // useEffect(() => {
+  //   let button = document.querySelector(".some-button");
+  //   // let item = document.querySelector(".some-button .round");
+  //   button.addEventListener("mouseenter", function (event) {
+  //     this.classList += " animate";
+  //     let buttonX = event.offsetX;
+  //     let buttonY = event.offsetY;
+  //     // if (buttonY < 24) {
+  //     //   item.style.top = 0 + "px";
+  //     // } else if (buttonY > 30) {
+  //     //   item.style.top = 48 + "px";
+  //     // }
+  //     // item.style.left = buttonX + "px";
+  //     // item.style.width = "1px";
+  //     // item.style.height = "1px";
+  //   });
+  //   button.addEventListener("mouseleave", function () {
+  //     // this.classList.remove("animate");
+  //     // let buttonX = event.offsetX;
+  //     // let buttonY = event.offsetY;
+  //     // if (buttonY < 24) {
+  //     //   item.style.top = 0 + "px";
+  //     // } else if (buttonY > 30) {
+  //     //   item.style.top = 48 + "px";
+  //     // }
+  //     // item.style.left = buttonX + "px";
+  //   });
+  // }, []);
   return (
     <Layout>
       <Head>
@@ -48,17 +46,16 @@ export default function Home() {
         <meta
           name="description"
           content="Pulzion is the annual flagship event organized by PICT ACM
-				Student Chapter (PASC). Pulzion consists of multiple events in
-				technical as well as non-technical domains including coding
-				competitions, mock placement interviews, business
-				management-based events, design and development based contests
-				and quizzing events."
+        Student Chapter (PASC). Pulzion consists of multiple events in
+        technical as well as non-technical domains including coding
+        competitions, mock placement interviews, business
+        management-based events, design and development based contests
+        and quizzing events."
         />
       </Head>
       <div
-        className={` flex flex-col items-center justify-center pb-5 ${
-          exploring ? "" : "relative"
-        }  `}
+        className={` flex flex-col items-center justify-center pb-5 ${exploring ? "" : "relative"
+          }  `}
         style={{
           paddingTop: "10vh",
           zIndex: 20,
@@ -80,25 +77,28 @@ export default function Home() {
           <div className="flex flex-row items-center justify-center gap-4 text-2xl font-bold md:gap-6 text-primaries-100 md:text-5xl">
             <IoCalendarOutline /> <p>3rd, 4th &amp; 5th May</p>
           </div>
-
           <div className="flex flex-col gap-6 xl:mt-5 sm:flex sm:flex-row">
-            <button
-              class="primary-button"
-              style={{
-                backgroundImage: "linear-gradient(to right, #172947c5, black)",
-                opacity: 0.95,
-                position: "relative",
-                borderWidth: "2px",
-                borderStyle: "solid",
-                borderRadius: "550px",
-                width: "250px",
-                height: "48px",
-                overflow: "hidden",
-                backgroundColor: "transparent",
-                textTransform: "uppercase",
-                color: "white",
-                fontWeight: "700px",
-              }}
+            {/* <button
+              // class="primary-button"
+              className={`${styles.animated} some-button`}
+              // style={{
+              //   clipPath: "polygon(12% 0, 0% 30%, 0 100%, 88% 100% , 100% 65%, 100% 0)",
+              // }}
+              // style={{                
+              //   backgroundImage: "linear-gradient(to right, #172947c5, black)",
+              //   opacity: 0.95,
+              //   position: "relative",
+              //   borderWidth: "2px",
+              //   borderStyle: "solid",
+              //   borderRadius: "550px",
+              //   width: "250px",
+              //   height: "48px",
+              //   overflow: "hidden",
+              //   backgroundColor: "transparent",
+              //   textTransform: "uppercase",
+              //   color: "white",
+              //   fontWeight: "700px",
+              // }}
               // className="bg-gradient-to-br from-[#172947c5] to-black opacity-95 relative border-2 border-solid rounded-[550px] w-[250px] h-[48px] overflow-hidden bg-transparent uppercase text-white text-sm font-bold"
               onClick={() =>
                 user?.id ? router.push("/events") : router.push("/register")
@@ -106,16 +106,28 @@ export default function Home() {
             >
               {user?.id ? "Register For Events" : "Register Now"}
               <span class="round" />
+            </button> */}
+            <button className={`${styles.animated} some-button`}
+              onClick={() =>
+                user?.id ? router.push("/events") : router.push("/register")
+              }
+            >            
+              <span className="italic text-xl tracking-normal">{user?.id ? "Register For Events" : "Register Now"}</span>
             </button>
-
-            <button
-              // className="relative border-2 border-solid border-primaries-500 rounded-[550px] w-[250px] h-[48px] overflow-hidden bg-transparent uppercase text-white text-sm font-bold"
-              class="primary-button"
-              className="bg-gradient-to-r hover:border-blue-800 from-[#172947c5] to-black opacity-95 relative border-2 border-solid rounded-[550px] w-[250px] h-[48px] overflow-hidden bg-transparent uppercase text-white text-sm font-bold"
+            {/* <button
+              // className="bg-gradient-to-r shadow-[#630834] hover:border-blue-800 from-[#172947c5] to-black opacity-95 relative border-2 border-solid rounded-[550px] w-[250px] h-[48px] overflow-hidden bg-transparent uppercase text-white text-sm font-bold"
+              // className={`${styles.animate} before:h-[100%] after:text-center relative z-20 after:absolute after:inset-[5px] after:py-5 after:bg-gradient-to-br from-[#172947c5] to-black after:bg-opacity-100 after:w-[96%] before:absolute before:w-[100%] before:shadow-[#630834a2] before:shadow-[20px_20px_5px_2px] before:bg-[#630834] before:inset-0 italic w-[250px] h-[70px] overflow-hidden bg-transparent uppercase text-white text-sm font-bold`}                
+              className={`${styles.animate} relative z-20 italic w-[250px] h-[70px] overflow-hidden bg-transparent uppercase text-white text-sm font-bold`}                
+              style={{
+                clipPath: "polygon(12% 0, 0% 30%, 0 100%, 88% 100% , 100% 65%, 100% 0)",
+              }}
               onClick={() => setExploring(() => !exploring)}
             >
               {exploring ? " Go back Main Page" : "Explore"}
               <span class="round" />
+            </button> */}
+            <button className={`${styles.animated}`} onClick={() => setExploring(() => !exploring)}>
+              <span className="italic text-xl tracking-normal">{exploring ? " Go back Main Page" : "Explore"}</span>
             </button>
           </div>
         </div>
