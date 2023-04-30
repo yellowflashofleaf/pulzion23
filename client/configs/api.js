5630e158e0854cb9a4c55732f8d8aa7fb31a0da1
PRATHAM MORE <pratham02more20@gmail.com>
2023-04-30 14:16:53 +0530

    api changed

--- client/configs/api.js
+++ client/configs/api.js
@@ -1,6 +1,6 @@
 const apiConfig = {
-  //url: "https://ems-test-server.onrender.com/",
-  url: "https://staging.api.pulzion.co.in",
+  url: "https://ems-test-server.onrender.com/",
+  //url: "https://staging.api.pulzion.co.in",
 };
 
 export default apiConfig;
